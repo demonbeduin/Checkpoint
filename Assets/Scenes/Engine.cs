04006eea0a9dc08df5cfb90252f9880489fbde27
demonbeduin <team.sad.info@gmail.com>
2019-05-26 16:23:14 +0300

    test 12

---
+++ Assets/Scenes/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    public Transform player;
+    public Transform target;
+    public int speed;
+
+    void Start()
+    {
+        player.rotation = Quaternion.identity;
+        target.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        //Vector3 relativePos = (target.transform.position - player.transform.position).normalized;
+        //Quaternion rotation = Quaternion.LookRotation(relativePos);
+        //target.transform.rotation = rotation;
+        target.transform.LookAt(player);
+    }
+}
