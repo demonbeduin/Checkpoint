aa18036c3c4bf89a1fb4484fcb5a1c7ef88b41c1
demonbeduin <team.sad.info@gmail.com>
2019-06-02 16:29:44 +0300

    test 16

---
+++ Assets/Scenes/Fire_point.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_point : MonoBehaviour
+{
+    public GameObject _prefab;
+    public float _speed;
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+            GameObject _bull = Instantiate(_prefab, transform.position, Quaternion.identity);
+            Rigidbody _rigidbody = _bull.GetComponent<Rigidbody>();
+
+            _rigidbody.velocity = _speed * transform.forward;
+
+            Destroy(_bull, 5F);
+        } 
+    }
+}
---
+++ Assets/Scenes/Joistic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joistic : MonoBehaviour
+{
+    [SerializeField]
+    private Transform joistic;
+    [SerializeField]
+    private float _speed = 5F;
+    private float _angel = 90F;
+
+    // --------------------------------------------------------------------------------------------
+    private void Awake()
+    {
+        
+    }
+    // --------------------------------------------------------------------------------------------
+    void Start()
+    {
+        
+    }
+    // --------------------------------------------------------------------------------------------
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * -_angel);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * _angel);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.forward * Time.deltaTime * -_speed);
+        }
+    }
+    // --------------------------------------------------------------------------------------------
+    private void FixedUpdate()
+    {
+        
+    }
+    // --------------------------------------------------------------------------------------------
+    private void LateUpdate()
+    {
+        
+    }
+}
