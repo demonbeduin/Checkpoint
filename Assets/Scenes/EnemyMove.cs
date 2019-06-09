148850556bb23838de99b4ec80aefd9762f00989
demonbeduin <team.sad.info@gmail.com>
2019-06-09 15:42:08 +0300

    test 21

--- Assets/Scenes/EnemyMove.cs
+++ Assets/Scenes/EnemyMove.cs
@@ -15,9 +15,12 @@ public class EnemyMove : MonoBehaviour
     void Update()
     {
         Vector3 _dir = (_playerTarget.transform.position - transform.position).normalized;
+        Quaternion _enemyRotation = Quaternion.LookRotation(_dir);
         transform.position += _dir * _movespeed * Time.deltaTime;
+        transform.rotation = _enemyRotation;
+
         //Vector3 _newdir = Vector3.RotateTowards(_playerTarget.transform.forward, _dir, _movespeed * Time.deltaTime, 0.0F);
-        //_playerTarget.transform.rotation = Quaternion.LookRotation(_newdir);
+        //_playerTarget.transform.rotation = Quaternion.LookRotation(_dir);
 
     }
 }
