043a59aae1bfae2aff14bc4fc572d4a668b7f2ec
demonbeduin <team.sad.info@gmail.com>
2019-06-09 15:04:46 +0300

    test 19

---
+++ Assets/Scenes/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject _prefab;
+
+    void Start()
+    {
+        //GameObject _target = Instantiate(_prefab, transform.position, Quaternion.identity);
+        InvokeRepeating("Res", 0, 5f);
+    }
+    private void Res()
+    {
+        GameObject obj = Instantiate(_prefab, transform.position, Quaternion.identity);
+        Destroy(obj, 10f);
+    }
+
+    void Update()
+    {
+                
+    }
+}
