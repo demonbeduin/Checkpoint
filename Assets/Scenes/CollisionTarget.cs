4d51c050e7816e8dc124c6650eb473ef6cf4f491
demonbeduin <team.sad.info@gmail.com>
2019-06-23 15:52:05 +0300

    test 27

--- Assets/Scenes/CollisionTarget.cs
+++ Assets/Scenes/CollisionTarget.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class CollisionTarget : MonoBehaviour
 {
     void Start()
     {
---
+++ Assets/Scenes/Triger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triger : MonoBehaviour
+{
+    private GameObject _targetTriger;
+
+    void Start()
+    {
+
+        _targetTriger = GameObject.FindWithTag("Light_5");
+        _targetTriger.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            print("OnTriggerEnter" + other.name);
+            _targetTriger.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            print("OnTriggerExit" + other.name);
+            _targetTriger.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
