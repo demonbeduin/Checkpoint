075427ae104ae62ae4178aaf7bc4f9d73e7bcc51
demonbeduin <team.sad.info@gmail.com>
2019-06-30 17:01:33 +0300

    test 31

--- Assets/Scenes/ForCameraRay.cs
+++ Assets/Scenes/ForCameraRay.cs
@@ -7,8 +7,9 @@ public class ForCameraRay : MonoBehaviour
     public Camera _camera;
     public GameObject _prefab;
     public GameObject _player;
-    public float _speed = 10f;
+    public float _speed = 20f;
     public float _rSpeed = 1f;
+    Vector3 _dir;
 
     void Start()
     {
@@ -27,14 +28,16 @@ public class ForCameraRay : MonoBehaviour
             {
                 print(hit.point);
 
+                _dir = (hit.point - _player.transform.position);
                 //_player.transform.position = Vector3.MoveTowards(_player.transform.position, hit.point, _movespeed * Time.deltaTime);
 
                 //Instantiate(_prefab, hit.point, Quaternion.identity);
-                if (Vector3.Distance(hit.point, _player.transform.position) > 1)
-                {
-                    Vector3 _dir = (hit.point - _player.transform.position).normalized;
-                    _player.transform.position += _dir * _speed * Time.deltaTime;
-                }
+            }
+            if (Vector3.Distance(hit.point, _player.transform.position) > 1)
+            {
+                Vector3 _newdir = Vector3.RotateTowards(_player.transform.forward, _dir, _rSpeed * Time.deltaTime, 0.0F);
+                _player.transform.rotation = Quaternion.LookRotation(_newdir);
+                _player.transform.position += _dir * _speed * Time.deltaTime;
             }
         }
     }
