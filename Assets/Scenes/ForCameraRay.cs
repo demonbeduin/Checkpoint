2e0c6ccd1dd9f7e2b1eb726531ea81f8633230f3
demonbeduin <team.sad.info@gmail.com>
2019-06-30 15:47:56 +0300

    test 29

---
+++ Assets/Scenes/ForCameraRay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForCameraRay : MonoBehaviour
+{
+    public Camera _camera;
+    public GameObject _prefab;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool collision = Physics.Raycast(ray.origin, ray.direction, out hit);
+
+        if (collision)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                print(hit.point);
+                //Instantiate(_prefab, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
